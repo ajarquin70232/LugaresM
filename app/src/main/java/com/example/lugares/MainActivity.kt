7d92aeeaef91adc79c0a0ff1e28193547abcb6fe
andresjarquinsalazar <ajarquin70232@ufide.ac.cr>
2022-12-08 20:22:32 -0600

    Commit 3

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.lugares.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(logoutInflater)
+        setContentView(binding.roat)
+        FirebaseAp.initializeApp(context: this)
+        auth= Firebase.auth
+
+        bindingn
     }
 }
